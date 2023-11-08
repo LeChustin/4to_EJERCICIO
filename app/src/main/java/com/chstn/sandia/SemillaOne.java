16615cf3d4d8a5f5653a4765a4b1da71c9e80368
LeChustin <jostinaldaz4@gmail.com>
2023-11-08 09:53:59 -0500

    segundo comit y esas cosas

---
+++ app/src/main/java/com/chstn/sandia/SemillaOne.java
@@ -0,0 +1,14 @@
+package com.chstn.sandia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SemillaOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semilla_one);
+    }
+}
